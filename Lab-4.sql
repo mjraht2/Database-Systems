5f705a80f535ffae4405ccaeeba5477a25eb6979
mjraht2 <Mrahtelli2>
2016-02-16 12:12:06 -0500

    Lab-4
    this lab was fantastic. it did not make me want to crawl into a corner,
    wither and die.

---
+++ Lab-4.sql
@@ -0,0 +1,281 @@
+﻿----------------------------------------------------------------------------------------
+-- Postgres create, load, and query script for CAP3.
+--
+-- SQL statements for the CAP3 database
+-- 
+-- Derived from the CAP examples in _Database Principles, Programming, and Performance_, 
+--   Second Edition by Patrick O'Neil and Elizabeth O'Neil
+-- 
+-- Modified several times by Alan G. Labouseur
+-- 
+-- Tested on Postgres 9.3.2    (For earlier versions, you may need
+--   to remove the "if exists" clause from the DROP TABLE commands.)
+----------------------------------------------------------------------------------------
+
+-- Connect to your Postgres server and set the active database to CAP3. Then . . .
+
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS agents;
+DROP TABLE IF EXISTS products;
+
+
+-- Customers --
+CREATE TABLE customers (
+  cid      char(4) not null,
+  name     text,
+  city     text,
+  discount decimal(5,2),
+ primary key(cid)
+);
+
+
+-- Agents --
+CREATE TABLE agents (
+  aid        char(3) not null,
+  name       text,
+  city       text,
+  commission decimal(5,2),
+ primary key(aid)
+);        
+
+
+-- Products --
+CREATE TABLE products (
+  pid      char(3) not null,
+  name     text,
+  city     text,
+  quantity integer,
+  priceUSD numeric(10,2),
+ primary key(pid)
+);
+
+
+-- Orders -- 
+CREATE TABLE orders (
+  ordnum   integer not null,
+  mon      char(3),    
+  cid      char(4) not null references customers(cid),
+  aid      char(3) not null references agents(aid),
+  pid      char(3) not null references products(pid),
+  qty      integer,
+  totalUSD numeric(12,2),
+ primary key(ordnum)
+);
+
+
+
+-- SQL statements for loading example data into the CAP3 database
+-- Connect to your Postgres server and set the active database to CAP3. Then . . .
+
+-- Customers --
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c001', 'Tiptop', 'Duluth', 10.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c002', 'Tyrell', 'Dallas', 12.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c003', 'Allied', 'Dallas', 8.50);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c004' ,'ACME' ,'Duluth', 8.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c005' ,'Weyland', 'Acheron', 0.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c006' ,'ACME' ,'Kyoto' ,0.00);
+
+
+-- Agents --
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a01', 'Smith', 'New York', 6.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a02', 'Jones', 'Newark', 6.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a03', 'Perry', 'Tokyo', 7.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a04', 'Gray', 'New York', 6.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a05', 'Otasi', 'Duluth', 5.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a06', 'Smith', 'Dallas', 5.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a08', 'Bond', 'London', 7.07 );
+
+
+-- Products --
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p01', 'comb', 'Dallas', 111400, 0.50 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p02', 'brush', 'Newark', 203000, 0.50 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p03', 'razor', 'Duluth', 150600, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p04', 'pen', 'Duluth', 125300, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p05', 'pencil', 'Dallas', 221400, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p06', 'folder','Dallas', 123100, 2.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p07', 'case', 'Newark', 100500, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p08', 'clip', 'Newark', 200600, 1.25 );
+
+
+-- Orders --
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1011, 'jan', 'c001', 'a01', 'p01', 1000, 450.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1013, 'jan', 'c002', 'a03', 'p03', 1000, 880.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1015, 'jan', 'c003', 'a03', 'p05', 1200, 1104.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1016, 'jan', 'c006', 'a01', 'p01', 1000, 500.00);
+  
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1017, 'feb', 'c001', 'a06', 'p03',  600, 540.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1018, 'feb', 'c001', 'a03', 'p04',  600, 540.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1019, 'feb', 'c001', 'a02', 'p02',  400, 180.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1020, 'feb', 'c006', 'a03', 'p07',  600, 600.00);
+  
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1021, 'feb', 'c004', 'a06', 'p01', 1000, 460.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1022, 'mar', 'c001', 'a05', 'p06',  400, 720.00);
+    
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1023, 'mar', 'c001', 'a04', 'p05',  500, 450.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1024, 'mar', 'c006', 'a06', 'p01',  800, 400.00);
+  
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1025, 'apr', 'c001', 'a05', 'p07',  800, 720.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1026, 'may', 'c002', 'a05', 'p03',  800, 740.00);
+
+
+-- SQL statements for displaying example data into the CAP3 database
+-- Connect to your Postgres server and set the active database to CAP3. Then . . .
+
+
+-- #1 - Get the cities of agents booking an order for a customer whose cid is 'c002'
+SELECT city
+FROM agents
+WHERE aid in (
+  SELECT aid
+  FROM orders
+  WHERE cid = 'c002'
+  );
+
+-- #2 - Get the ids of products ordered through any agent who takes at least one order 
+-- from a customer in Dallas, sorted by pid from highest to lowest.
+SELECT distinct pid
+FROM orders
+WHERE aid in (
+  SELECT aid
+  FROM orders
+  WHERE cid in (
+    SELECT cid
+    FROM customers
+    WHERE city = 'Dallas'
+  )
+)
+ORDER BY pid DESC;
+
+-- #3 - Find the ids and names of customers who did not place an order through agent a01.
+SELECT cid, name
+FROM customers
+WHERE cid not in (
+  SELECT distinct cid
+  FROM orders
+  WHERE aid = 'a01'
+  );
+
+-- #4 Get the ids of customers who ordered both product p01 and p07.
+SELECT cid
+FROM customers
+WHERE cid in (
+  SELECT distinct cid
+  FROM orders
+  WHERE pid = 'p01' and cid in (
+    SELECT cid
+    FROM orders
+    WHERE pid = 'p07'
+    )
+);
+
+-- #5 Get the ids of products not ordered by any customers who placed any order through
+-- agent a07 in pid order from highest to lowest.
+SELECT distinct pid
+FROM products
+WHERE pid not in (
+  SELECT pid
+  FROM orders
+  WHERE aid = 'a07'
+)
+ORDER BY pid DESC;
+
+-- #6 Get the name, discounts, and city for all customers who place orders through agents
+-- in London or New York.
+SELECT name, discount, city
+FROM customers
+WHERE cid in (
+  SELECT distinct cid
+  FROM orders
+  WHERE aid in (
+    SELECT aid
+    FROM agents
+    WHERE city = 'London' or city = 'New York'
+  )
+);
+
+--#7 Get all the customers who have the same discount as that of any customers in Dallas
+-- or London. *******************************
+SELECT *
+FROM customers
+WHERE cid not in (
+  SELECT cid
+  FROM customers
+  WHERE city = 'Dallas' or city = 'London'
+)
+AND discount in (
+  SELECT discount
+  FROM customers
+  WHERE city = 'Dallas' or city = 'London'
+);
+
+--#8 Tell me about check constraints: What are they? What are they good for?
+-- Constraints are typically associated with databases and define certain properties.
+-- Constraints help with user error and prevent users from imputting a command that
+-- does not work.
+-- Constraints also help with users not being able to delete data from the database.
+-- If you wanted to delete something from the database that is needed in another
+-- table.
+-- They will prevent tears and crying from students.
