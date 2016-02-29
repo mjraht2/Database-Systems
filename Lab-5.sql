9ac846fcc080200869615f9df6053ba1e937a6f5
mjraht2 <Mrahtelli2>
2016-02-28 21:50:52 -0500

    Lab-5

---
+++ Lab-5.sql
@@ -0,0 +1,237 @@
+﻿----------------------------------------------------------------------------------------
+-- Postgres create, load, and query script for CAP3.
+--
+-- SQL statements for the CAP3 database
+-- 
+-- Derived from the CAP examples in _Database Principles, Programming, and Performance_, 
+--   Second Edition by Patrick O'Neil and Elizabeth O'Neil
+-- 
+-- Modified several times by Alan G. Labouseur
+-- 
+-- Tested on Postgres 9.3.2    (For earlier versions, you may need
+--   to remove the "if exists" clause from the DROP TABLE commands.)
+----------------------------------------------------------------------------------------
+
+-- Connect to your Postgres server and set the active database to CAP3. Then . . .
+
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS agents;
+DROP TABLE IF EXISTS products;
+
+
+-- Customers --
+CREATE TABLE customers (
+  cid      char(4) not null,
+  name     text,
+  city     text,
+  discount decimal(5,2),
+ primary key(cid)
+);
+
+
+-- Agents --
+CREATE TABLE agents (
+  aid        char(3) not null,
+  name       text,
+  city       text,
+  commission decimal(5,2),
+ primary key(aid)
+);        
+
+
+-- Products --
+CREATE TABLE products (
+  pid      char(3) not null,
+  name     text,
+  city     text,
+  quantity integer,
+  priceUSD numeric(10,2),
+ primary key(pid)
+);
+
+
+-- Orders -- 
+CREATE TABLE orders (
+  ordnum   integer not null,
+  mon      char(3),    
+  cid      char(4) not null references customers(cid),
+  aid      char(3) not null references agents(aid),
+  pid      char(3) not null references products(pid),
+  qty      integer,
+  totalUSD numeric(12,2),
+ primary key(ordnum)
+);
+
+
+
+-- SQL statements for loading example data into the CAP3 database
+-- Connect to your Postgres server and set the active database to CAP3. Then . . .
+
+-- Customers --
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c001', 'Tiptop', 'Duluth', 10.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c002', 'Tyrell', 'Dallas', 12.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c003', 'Allied', 'Dallas', 8.50);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c004' ,'ACME' ,'Duluth', 8.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c005' ,'Weyland', 'Acheron', 0.00);
+
+INSERT INTO Customers( cid, name, city, discount )
+  VALUES('c006' ,'ACME' ,'Kyoto' ,0.00);
+
+
+-- Agents --
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a01', 'Smith', 'New York', 6.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a02', 'Jones', 'Newark', 6.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a03', 'Perry', 'Tokyo', 7.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a04', 'Gray', 'New York', 6.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a05', 'Otasi', 'Duluth', 5.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a06', 'Smith', 'Dallas', 5.00 );
+
+INSERT INTO Agents( aid, name, city, commission )
+  VALUES('a08', 'Bond', 'London', 7.07 );
+
+
+-- Products --
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p01', 'comb', 'Dallas', 111400, 0.50 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p02', 'brush', 'Newark', 203000, 0.50 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p03', 'razor', 'Duluth', 150600, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p04', 'pen', 'Duluth', 125300, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p05', 'pencil', 'Dallas', 221400, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p06', 'folder','Dallas', 123100, 2.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p07', 'case', 'Newark', 100500, 1.00 );
+
+INSERT INTO Products( pid, name, city, quantity, priceUSD )
+  VALUES('p08', 'clip', 'Newark', 200600, 1.25 );
+
+
+-- Orders --
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1011, 'jan', 'c001', 'a01', 'p01', 1000, 450.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1013, 'jan', 'c002', 'a03', 'p03', 1000, 880.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1015, 'jan', 'c003', 'a03', 'p05', 1200, 1104.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1016, 'jan', 'c006', 'a01', 'p01', 1000, 500.00);
+  
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1017, 'feb', 'c001', 'a06', 'p03',  600, 540.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1018, 'feb', 'c001', 'a03', 'p04',  600, 540.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1019, 'feb', 'c001', 'a02', 'p02',  400, 180.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1020, 'feb', 'c006', 'a03', 'p07',  600, 600.00);
+  
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1021, 'feb', 'c004', 'a06', 'p01', 1000, 460.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1022, 'mar', 'c001', 'a05', 'p06',  400, 720.00);
+    
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1023, 'mar', 'c001', 'a04', 'p05',  500, 450.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1024, 'mar', 'c006', 'a06', 'p01',  800, 400.00);
+  
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1025, 'apr', 'c001', 'a05', 'p07',  800, 720.00);
+
+INSERT INTO Orders( ordnum, mon, cid, aid, pid, qty, totalUSD )
+  VALUES(1026, 'may', 'c002', 'a05', 'p03',  800, 740.00);
+
+--1 - Show the cities of agents booking an order for a customer whose id is 'c002'. Use	
+-- joins; no subqueries.
+SELECT agents.city
+FROM customers, agents, orders
+WHERE orders.aid = agents.aid
+  AND orders.cid = customers.aid
+  AND customers.cid = 'c002';
+
+--2 - Show the id of products ordered through any agent who makes at least one order for	
+-- a customer in Dallas, sorted by pid from highest to lowest. Use joins; no subqueries.
+SELECT products.pid
+FROM orders INNER JOIN agents ON orders.aid = agents.aid
+            INNER JOIN customers ON orders.cid = customers.cid
+            INNER JOIN products ON orders.pid = products.pid
+            WHERE customers.city = 'Dallas';
+
+--3 - Show the names of customers who have never placed an order. Use a subquery.
+SELECT Name
+FROM customers
+WHERE cid NOT IN (SELECT cid
+                  FROM orders
+                  );
+
+
+--4 - Show the names of customers who have never placed an order. Use an outer join.
+SELECT name
+FROM customers
+WHERe cid NOT IN (SELECT customers.cid
+                  FROM customers RIGHT OUTER JOIN orders ON orders.cid = customers.cid
+                  );
+
+--5 - Show the names of customers who placed at least one order through an agent in their	
+-- own city, along with those agent(s') names.
+SELECT agents.name, customers.name
+FROM  orders INNER JOIN customers ON orders.cid = customers.cid
+                               INNER JOIN agents ON orders.aid = agents.aid
+                               WHERE agents.city = customers.city;
+
+--6 - Show the names of customers and agents living in the same city, alongg with the name	
+-- of the shared city, regardless of whether or not the customer has ever placed an order	
+-- with that agent.
+SELECT customers.name, customers.city, agents.name, agent.city
+FROM customers, agents
+WHERE customers.city = agents.city;
+
+--7 Show the name and city of customers who live in the city that makes the fewest
+--different kinds of products.
+SELECT NAME, city
+FROM customers
+WHERE city IN (SELECT city
+               FROM products
+               GROUP BY city
+               ORDER BY COUNT(pid)
+               LIMIT 1
+               );
